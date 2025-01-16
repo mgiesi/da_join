c49a9c723a24fafc1422ae38b3667ccec16a4c03
Markus Giesinger <mgiesi@gmail.com>
2025-01-16 19:15:50 +0100

    Add js part for summary page (greeting)

---
+++ assets/js/summary.js
@@ -0,0 +1,37 @@
+/** Init function for the summary page */
+function initSummary() {
+    refreshGreeting();
+}
+
+function refreshGreeting() {
+    const username = getUserName();
+    refreshGreetingText(username);
+    refreshGreetingName(username);
+}
+
+function refreshGreetingText(username) {
+    const greetingTextRef = document.getElementById("summary-greeting-text");
+    const hours = new Date().getHours();
+
+    if (hours < 11) {
+        greetingTextRef.innerHTML = "Good morning"
+    } else if (hours < 17) {
+        greetingTextRef.innerHTML = "Good afternoon"
+    } else {
+        greetingTextRef.innerHTML = "Good evening"
+    }
+
+    if (username !== null) {
+        greetingTextRef.innerHTML += ",";
+    }
+}
+
+function refreshGreetingName(username = "") {
+    const greetingNameRef = document.getElementById("summary-greeting-name");
+    greetingNameRef.innerHTML = username;
+}
+
+function getUserName() {
+    //todo read current logged in user from internal storage
+    return "Markus Giesinger";
+}
