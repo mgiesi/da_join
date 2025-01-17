7c3e9f12e81178b82971d88a7eacdf3ce55b9515
Heuft <max.heuft@rocketmail.com>
2025-01-17 12:59:32 +0100

    m

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,15 @@
 function navigateToSignUp() {
   window.location.href = "signUp.html";
 }
+
+function navigateToSummary() {
+  window.location.href = "summary.html";
+}
+
+function showMessage() {
+  const overlay = document.getElementById("overlay");
+  overlay.style.display = "flex";
+  setTimeout(() => {
+    window.location.href = "index.html";
+  }, 2000);
+}
