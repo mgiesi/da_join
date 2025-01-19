458c544862c86c131c69c8b05995284bbd7f27ee
Heuft <max.heuft@rocketmail.com>
2025-01-19 10:44:32 +0100

    Letzte Änderungen

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,9 +12,16 @@ function navigateToSummary() {
 
 function showMessage(event) {
   event.preventDefault();
-  document.getElementById("overlay").classList.remove("dNone");
-  overlay.classList.add("animate");
-  setTimeout(function () {
-    window.location.href = "index.html";
-  }, 2000);
+  let checkbox = document.getElementById("checkbox");
+  let dialogSignUp = document.getElementById("dialogSignUp");
+
+  if (checkbox && checkbox.checked) {
+    document.getElementById("overlay").classList.remove("dNone");
+    overlay.classList.add("animate");
+    setTimeout(function () {
+      window.location.href = "index.html";
+    }, 2000);
+  } else {
+    dialogSignUp.classList.remove("dNone");
+  }
 }
