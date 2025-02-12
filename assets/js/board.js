31176515dc6c51d1b191ecdccd9fddeed4381125
Heuft <max.heuft@rocketmail.com>
2025-02-12 14:57:07 +0100

    Onclick Task-Details without Variables

--- assets/js/board.js
+++ assets/js/board.js
@@ -3,55 +3,59 @@ const boardNames = ["todo", "inprogress", "awaitfeedback", "done"];
 let filterText;
 
 async function initBoard() {
-    filterText = "";
-    await renderBoardContainer();
-    await renderTasks();
+  filterText = "";
+  await renderBoardContainer();
+  await renderTasks();
 }
 
 function updateFilter() {
-    const filterInputRef = document.getElementById("board-filter-text");
-    filterText = filterInputRef.value;
-    renderTasks();
+  const filterInputRef = document.getElementById("board-filter-text");
+  filterText = filterInputRef.value;
+  renderTasks();
 }
 
 function resetFilter() {
-    const filterInputRef = document.getElementById("board-filter-text");
-    filterInputRef.value = "";
-    filterText = "";
-    renderTasks();
+  const filterInputRef = document.getElementById("board-filter-text");
+  filterInputRef.value = "";
+  filterText = "";
+  renderTasks();
 }
 
 async function renderBoardContainer() {
-    const boards = await getBoards();
-    const boardContent = document.getElementById("boards-container");
-    boardContent.innerHTML = "";
-    for (const boardName of boardNames) {
-        boardContent.innerHTML += displayBoardContainer(boards[boardName]);
-    }
+  const boards = await getBoards();
+  const boardContent = document.getElementById("boards-container");
+  boardContent.innerHTML = "";
+  for (const boardName of boardNames) {
+    boardContent.innerHTML += displayBoardContainer(boards[boardName]);
+  }
 }
 
 async function renderTasks() {
-    const [boards, tasks, contacts] = await Promise.all([
-        getBoards(),
-        getTasks(filterText),
-        getContacts(),
-    ]);
-    for (const boardName of boardNames) {
-        const boardContent = document.getElementById(
-            "board-container-" + boardName
-        );
-        boardContent.innerHTML = displayBoardTasks(
-            boards[boardName],
-            tasks,
-            contacts
-        );
-    }
+  const [boards, tasks, contacts] = await Promise.all([
+    getBoards(),
+    getTasks(filterText),
+    getContacts(),
+  ]);
+  for (const boardName of boardNames) {
+    const boardContent = document.getElementById(
+      "board-container-" + boardName
+    );
+    boardContent.innerHTML = displayBoardTasks(
+      boards[boardName],
+      tasks,
+      contacts
+    );
+  }
 }
 
 function toggleAddTaskOverlay() {
-    let overlay = document.getElementById("overlayAddTask");
-    overlay.classList.toggle("dNone");
-    overlay.innerHTML = getAddTaskOverlay();
+  let overlay = document.getElementById("overlayAddTask");
+  overlay.classList.toggle("dNone");
+  overlay.innerHTML = getAddTaskOverlay();
 }
 
-
+function toggleTaskDetails() {
+  let overlay = document.getElementById("taskDetails");
+  overlay.classList.toggle("dNone");
+  overlay.innerHTML = getTaskDetails();
+}
--- assets/templates/boardTemplate.js
+++ assets/templates/boardTemplate.js
@@ -1,6 +1,8 @@
 function displayBoardContainer(board) {
-    let addTaskContent = board.addtasks ? `<div onclick="toggleAddTaskOverlay()" class="board-container-titlebox-addtask d-flex justify-content-center align-items-center">+</div>` : "";
-    return `
+  let addTaskContent = board.addtasks
+    ? `<div onclick="toggleAddTaskOverlay()" class="board-container-titlebox-addtask d-flex justify-content-center align-items-center">+</div>`
+    : "";
+  return `
         <div class="board-container" ondrop="moveTaskTo('${board.id}')" ondragover="allowDrop(event)">
             <div class="board-container-titlebox d-flex justify-content-between">
                 <span class="board-container-titlebox-title f10">${board.name}</span>
@@ -14,18 +16,16 @@ function displayBoardContainer(board) {
 }
 
 function displayBoardTasks(board, tasks, contacts) {
-    const taskCount = board && board.tasks
-        ? Object.keys(board.tasks).length
-        : 0;
-    if (taskCount <= 0) {
-        return displayEmptyBoard(board);
-    } else {
-        return displayTasks(board, tasks, contacts);
-    }
+  const taskCount = board && board.tasks ? Object.keys(board.tasks).length : 0;
+  if (taskCount <= 0) {
+    return displayEmptyBoard(board);
+  } else {
+    return displayTasks(board, tasks, contacts);
+  }
 }
 
 function displayEmptyBoard(board) {
-    return `
+  return `
             <div class="board-tasks-notasks d-flex justify-content-center align-items-center f3">
                 No tasks ${board.name}
             </div>
@@ -33,15 +33,17 @@ function displayEmptyBoard(board) {
 }
 
 function displayTasks(board, tasks, contacts) {
-    let htmlContent = "";
+  let htmlContent = "";
 
-    Object.keys(board.tasks).forEach(taskId => {
-        const task = tasks[taskId];
-        if (task === undefined || task === null) {
-            return;
-        }
-        htmlContent += `
-            <div class="board-task-container" draggable="true" ondragstart="startTaskDragging('${board.id}', '${taskId}')">
+  Object.keys(board.tasks).forEach((taskId) => {
+    const task = tasks[taskId];
+    if (task === undefined || task === null) {
+      return;
+    }
+    htmlContent += `
+            <div onclick="toggleTaskDetails()" class="board-task-container" draggable="true" ondragstart="startTaskDragging('${
+              board.id
+            }', '${taskId}')">
                 <div class="d-flex mb-24">
                     ${displayTaskType(task.category)}
                 </div>
@@ -58,32 +60,33 @@ function displayTasks(board, tasks, contacts) {
                     <div class="board-task-contacts">
                         ${displayAssignedTo(task, contacts)}
                     </div>
-                    <img class="board-task-category" src="./assets/icons/prio-${task.prio}.svg" alt="">
+                    <img class="board-task-category" src="./assets/icons/prio-${
+                      task.prio
+                    }.svg" alt="">
                 </div>
             </div>
         `;
-    });
-    return htmlContent;
+  });
+  return htmlContent;
 }
 
 function displayTaskType(taskCategory) {
-    if ("userstory" === taskCategory) {
-        return `<div class="board-task-type task-type-userstory f3">User Story</div>`;
-    } else {
-        return `<div class="board-task-type task-type-technicaltask f3">Technical Task</div>`;
-    }
+  if ("userstory" === taskCategory) {
+    return `<div class="board-task-type task-type-userstory f3">User Story</div>`;
+  } else {
+    return `<div class="board-task-type task-type-technicaltask f3">Technical Task</div>`;
+  }
 }
 
 function displaySubTasks(task) {
-    const subTasksCount = task && task.subtasks
-        ? Object.keys(task.subtasks).length
-        : 0;
-    if (subTasksCount <= 0) {
-        return "";
-    } else {
-        const subTasksDone = getSubTasksDoneCount(task, subTasksCount);
-        const subTasksDonePercent = subTasksDone / subTasksCount * 100;
-        return `
+  const subTasksCount =
+    task && task.subtasks ? Object.keys(task.subtasks).length : 0;
+  if (subTasksCount <= 0) {
+    return "";
+  } else {
+    const subTasksDone = getSubTasksDoneCount(task, subTasksCount);
+    const subTasksDonePercent = (subTasksDone / subTasksCount) * 100;
+    return `
             <div class="board-task-subtasks mb-24 d-flex align-items-center">
                 <div class="board-subtasks-bar">
                     <div style="width: ${subTasksDonePercent}%" class="board-subtasks-bar-value"></div>
@@ -91,48 +94,49 @@ function displaySubTasks(task) {
                 <span id="class="f11">${subTasksDone}/${subTasksCount} Subtasks</span>
             </div>
         `;
-    }
+  }
 }
 
 function getSubTasksDoneCount(task, subTasksCount) {
-    let subTasksDoneCount = 0;
-    for (let subTaskIdx = 0; subTaskIdx < subTasksCount; subTaskIdx++) {
-        if (task.subtasks["subtask" + (subTaskIdx + 1)].done) {
-            subTasksDoneCount++;
-        }
+  let subTasksDoneCount = 0;
+  for (let subTaskIdx = 0; subTaskIdx < subTasksCount; subTaskIdx++) {
+    if (task.subtasks["subtask" + (subTaskIdx + 1)].done) {
+      subTasksDoneCount++;
     }
-    return subTasksDoneCount;
+  }
+  return subTasksDoneCount;
 }
 
 function displayAssignedTo(task, contacts) {
-    const contactsCount = task && task.assignedTo
-        ? Object.keys(task.assignedTo).length
-        : 0;
-    if (contactsCount <= 0) {
-        return "";
-    } else {
-        let contactsContent = "";
-        Object.keys(task.assignedTo).forEach(contactId => {
-            const contact = contacts[contactId];
-            if (contact === undefined || contact === null) {
-                return;
-            } else {
-                contactsContent += displayContact(contact);
-            }
-        });
+  const contactsCount =
+    task && task.assignedTo ? Object.keys(task.assignedTo).length : 0;
+  if (contactsCount <= 0) {
+    return "";
+  } else {
+    let contactsContent = "";
+    Object.keys(task.assignedTo).forEach((contactId) => {
+      const contact = contacts[contactId];
+      if (contact === undefined || contact === null) {
+        return;
+      } else {
+        contactsContent += displayContact(contact);
+      }
+    });
 
-        return contactsContent;
-    }
+    return contactsContent;
+  }
 }
 
 function displayContact(contact) {
-    return `
-        <div class="task-contact f11" style="background: ${contact.avatarColor}">${getShortcutName(contact)}</div >
+  return `
+        <div class="task-contact f11" style="background: ${
+          contact.avatarColor
+        }">${getShortcutName(contact)}</div >
         `;
 }
 
 function getAddTaskOverlay() {
-    return `
+  return `
     <div class="overlay-addTask">
               
           <div w3-include-html=".\assets\templates\side-menu.html"></div>
@@ -242,4 +246,23 @@ function getAddTaskOverlay() {
       </div>
             </div>
           `;
+}
+
+function getTaskDetails() {
+  return `
+    <div class="topDetails">
+            <div class="board-task-type task-type-userstory f3">User Story</div><div onclick="toggleTaskDetails()" class="close-btn">×</div></div>
+            <div class="taskHeadline"><h1 class="f1">Kochwelt Page & Recipe Recommender</h1></div>
+            <div class="description f2"><p>Build start page recipe recommendation.</p></div>
+            <div class="dueDate"><p class="f2">Due Date: </p><p class="f2">10/05/2023</p></div>
+            <div class="priority"><p class="f2">Priority: </p><p class="f2">Medium</p></div>
+            <div class="assignedTo"><p class="topAssigned">Assigned To:</p><div class="assignedContacts"><div class="taskContactDetails">Emmanuel Mauer</div>
+            <div class="taskContactDetails">Marcel Bauer</div>
+            <div class="taskContactDetails">Anton Mayer</div></div></div>
+            <div class="subTasks"><p class="topAssigned">Subtasks</p><div class="checkedSubTasks"><div class="oneSubTask">
+                <input type="checkbox"><p class="f3">Implemented Recipe Recommendation</p></div><div class="oneSubTask">
+                    <input type="checkbox"><p class="f3">Start Page Layout</p></div></div></div>
+                    <div class="detailsButton"><button class="endBtn"><img class="detailsImgBtn" src="./assets/icons/delete.svg" alt="">Delete</button>
+                        <button class="endBtn"><img class="detailsImgBtn" src="./assets/icons/edit.svg" alt="">Edit</button></div>
+    `;
 }
