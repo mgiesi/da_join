43d3df610916a571c4d3a0d999de0e457c2b5dc0
Heuft <max.heuft@rocketmail.com>
2025-02-23 15:28:06 +0100

    Legal Side Responsive + Console Error Solved

---
+++ assets/js/signUp.js
@@ -0,0 +1,204 @@
+let users = [
+  { name: "Markus Giesinger", email: "test@web.de", password: "test" },
+];
+
+/**
+ * Navigates to the sign-up page.
+ */
+function navigateToSignUp() {
+  window.location.href = "signUp.html";
+}
+
+/**
+ * Navigates to the index page.
+ */
+function navigateToIndex() {
+  window.location.href = "index.html";
+}
+
+/**
+ * Navigates to the summary page.
+ */
+function navigateToSummary() {
+  window.location.href = "summary.html";
+}
+
+/**
+ * Shows a message upon user interaction with a checkbox.
+ * If the checkbox is checked, the user is redirected to the index page.
+ */
+function showMessage() {
+  let checkbox = document.getElementById("checkbox");
+  let dialogSignUp = document.getElementById("dialogSignUp");
+
+  if (checkbox && checkbox.checked) {
+    document.getElementById("overlay").classList.remove("dNone");
+    overlay.classList.add("animate");
+    setTimeout(function () {
+      window.location.href = "index.html";
+    }, 2000);
+  } else {
+    dialogSignUp.classList.remove("dNone");
+  }
+}
+
+/**
+ * Logs in as a guest by setting the user as "Guest" and navigating to the summary page.
+ */
+function loginAsGuest() {
+  localStorage.setItem("activeUser", "Guest");
+  navigateToSummary();
+}
+
+/**
+ * Handles the login process by validating user credentials and navigating to the summary page if successful.
+ */
+async function login() {
+  const email = document.getElementById("inputMail").value;
+  const password = document.getElementById("inputLock").value;
+
+  const dbUrl =
+    "https://da-join-629d2-default-rtdb.europe-west1.firebasedatabase.app/user.json";
+
+  try {
+    const users = await fetchUserData(dbUrl);
+    if (!users) return console.log("Keine Benutzer gefunden.");
+
+    const user = findUser(users, email, password);
+    user ? handleLoginSuccess(user) : handleLoginFailure();
+  } catch (error) {
+    handleLoginError(error);
+  }
+}
+
+/**
+ * Fetches user data from the specified URL.
+ */
+async function fetchUserData(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Fehler beim Abrufen der Benutzerdaten");
+  }
+  return response.json();
+}
+
+/**
+ * Finds a user by email and password from the list of users.
+ */
+function findUser(users, email, password) {
+  return Object.values(users).find(
+    (u) => u.email === email && u.password === password
+  );
+}
+
+/**
+ * Handles successful login by setting the active user in localStorage and navigating to the summary page.
+ */
+function handleLoginSuccess(user) {
+  localStorage.setItem("activeUser", user.email);
+  navigateToSummary();
+}
+
+/**
+ * Handles failed login by showing the login error dialog and highlighting invalid inputs.
+ */
+function handleLoginFailure() {
+  localStorage.removeItem("activeUser");
+  document.getElementById("dialogLogin").classList.remove("dNone");
+  redInput();
+}
+
+/**
+ * Handles login errors and logs the error message to the console.
+ */
+function handleLoginError(error) {
+  localStorage.removeItem("activeUser");
+  console.error("Fehler:", error);
+}
+
+/**
+ * Adds a new user by validating the input and saving the user to the database.
+ */
+async function addUser() {
+  const { name, email, password, confirmPassword } = getUserInput();
+  if (!validateInput(name, email, password, confirmPassword)) return;
+  if (!validatePassword(password, confirmPassword)) return;
+
+  const newUser = { name, email, password };
+  await saveUser(newUser);
+}
+
+/**
+ * Retrieves the user input from the sign-up form.
+ */
+function getUserInput() {
+  return {
+    name: document.getElementById("inputName").value,
+    email: document.getElementById("inputMail").value,
+    password: document.getElementById("inputLock").value,
+    confirmPassword: document.getElementById("inputConfirm").value,
+  };
+}
+
+/**
+ * Validates the user input fields for name, email, password, and confirmPassword.
+ */
+function validateInput(name, email, password, confirmPassword) {
+  if (!name || !email || !password || !confirmPassword) {
+    document.getElementById("FieldsSignUp").classList.remove("dNone");
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Validates if the password and confirmPassword match.
+ */
+function validatePassword(password, confirmPassword) {
+  if (password !== confirmPassword) {
+    document.getElementById("passwordSignUp").classList.remove("dNone");
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Saves a new user to the database.
+ */
+async function saveUser(newUser) {
+  try {
+    const userCount = await getUserCount();
+    const newUserKey = `user${userCount + 1}`;
+    await saveToDatabase(newUser, newUserKey);
+    showMessage();
+  } catch (error) {
+    console.error("Error adding user:", error);
+  }
+}
+
+/**
+ * Gets the total count of users in the database.
+ */
+async function getUserCount() {
+  const response = await fetch(
+    "https://da-join-629d2-default-rtdb.europe-west1.firebasedatabase.app/user.json"
+  );
+  if (!response.ok) throw new Error(`Fetch error: ${response.status}`);
+  const users = await response.json();
+  return users ? Object.keys(users).length : 0;
+}
+
+/**
+ * Saves a user to the Firebase database.
+ */
+async function saveToDatabase(user, key) {
+  const response = await fetch(
+    `https://da-join-629d2-default-rtdb.europe-west1.firebasedatabase.app/user/${key}.json`,
+    {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    }
+  );
+  if (!response.ok) throw new Error(`Save error: ${response.status}`);
+}
