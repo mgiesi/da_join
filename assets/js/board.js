e75f027ff7431f052fad8acfca23b15a8e381af0
Heuft <max.heuft@rocketmail.com>
2025-03-05 12:40:53 +0100

    Resp AddTask Complete + Board Scroll X add

--- assets/js/board.js
+++ assets/js/board.js
@@ -17,9 +17,9 @@ async function initBoard() {
 
 function modifyAddTask() {
   addOverlayClickListeners();
-  const btnCancel = document.getElementById('btn-add-task-clear');
-  const textNode = Array.from(btnCancel.childNodes).find(node => 
-    node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== ""
+  const btnCancel = document.getElementById("btn-add-task-clear");
+  const textNode = Array.from(btnCancel.childNodes).find(
+    (node) => node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== ""
   );
   if (textNode) {
     textNode.textContent = "Cancel";
@@ -28,9 +28,9 @@ function modifyAddTask() {
 }
 
 function addOverlayClickListeners() {
-  const overlays = document.querySelectorAll('.overlay-background');
-  overlays.forEach(function(overlay) {
-    overlay.addEventListener('click', function (e) {
+  const overlays = document.querySelectorAll(".overlay-background");
+  overlays.forEach(function (overlay) {
+    overlay.addEventListener("click", function (e) {
       if (e.target === this) {
         hideOverlays();
       }
@@ -53,9 +53,11 @@ function initSearchFilter() {
  */
 function updateFilterIcon() {
   const filterInputRef = document.getElementById("board-filter-text");
-  const filterInputIconRef = document.getElementById("board-filter-text-icon");  
+  const filterInputIconRef = document.getElementById("board-filter-text-icon");
   const filterInput2Ref = document.getElementById("board-filter-text2");
-  const filterInputIcon2Ref = document.getElementById("board-filter-text-icon2");
+  const filterInputIcon2Ref = document.getElementById(
+    "board-filter-text-icon2"
+  );
   filterInputIconRef.src = filterInputRef.value
     ? "./assets/icons/cancel.svg"
     : "./assets/icons/search.svg";
@@ -138,14 +140,14 @@ async function renderTasks() {
  * Updates the overlay content with the add task overlay template.
  */
 function showAddTaskOverlay() {
-  const btnSubmit = document.getElementById('btn-add-task-submit');
-  const textNode = Array.from(btnSubmit.childNodes).find(node => 
-    node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== ""
+  const btnSubmit = document.getElementById("btn-add-task-submit");
+  const textNode = Array.from(btnSubmit.childNodes).find(
+    (node) => node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== ""
   );
   if (textNode) {
     textNode.textContent = "Create Task";
   }
-  const addTaskForm = document.getElementById('add-task-form');
+  const addTaskForm = document.getElementById("add-task-form");
   addTaskForm.onsubmit = handleFormSubmit;
   resetTaskDetails();
   let overlay = document.getElementById("overlayAddTask");
@@ -153,21 +155,39 @@ function showAddTaskOverlay() {
 }
 
 /**
+ * Removed the visibility of the add task overlay.
+ */
+function removeAddTaskOverlay() {
+  const btnSubmit = document.getElementById("btn-add-task-submit");
+  const textNode = Array.from(btnSubmit.childNodes).find(
+    (node) => node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== ""
+  );
+  if (textNode) {
+    textNode.textContent = "Create Task";
+  }
+  const addTaskForm = document.getElementById("add-task-form");
+  addTaskForm.onsubmit = handleFormSubmit;
+  resetTaskDetails();
+  let overlay = document.getElementById("overlayAddTask");
+  overlay.classList.add("dNone");
+}
+
+/**
  * Shows the add task overlay.
- * 
+ *
  * @param {string} boardName name of the board where to add the new task
  */
 function showAddTaskOverlay(boardName) {
   addTaskToBoardName = boardName;
 
-  const btnSubmit = document.getElementById('btn-add-task-submit');
-  const textNode = Array.from(btnSubmit.childNodes).find(node => 
-    node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== ""
+  const btnSubmit = document.getElementById("btn-add-task-submit");
+  const textNode = Array.from(btnSubmit.childNodes).find(
+    (node) => node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== ""
   );
   if (textNode) {
     textNode.textContent = "Create Task";
   }
-  const addTaskForm = document.getElementById('add-task-form');
+  const addTaskForm = document.getElementById("add-task-form");
   addTaskForm.onsubmit = handleFormSubmit;
   resetTaskDetails();
   let overlay = document.getElementById("overlayAddTask");
@@ -178,8 +198,8 @@ function showAddTaskOverlay(boardName) {
  * Hides all overlays.
  */
 function hideOverlays() {
-  const overlays = document.querySelectorAll('.overlay-background');
-  overlays.forEach(function(overlay) {
+  const overlays = document.querySelectorAll(".overlay-background");
+  overlays.forEach(function (overlay) {
     overlay.classList.add("dNone");
   });
 }
@@ -212,13 +232,13 @@ async function toggleEditTaskDetails(taskId) {
   const [task, contacts] = await Promise.all([getTask(taskId), getContacts()]);
 
   loadTaskDetails(taskId, task, contacts);
-  const addTaskForm = document.getElementById('add-task-form');
-  addTaskForm.onsubmit = function(event) {
+  const addTaskForm = document.getElementById("add-task-form");
+  addTaskForm.onsubmit = function (event) {
     handleFormSubmit4Edit(event, taskId);
   };
-  const btnSubmit = document.getElementById('btn-add-task-submit');
-  const textNode = Array.from(btnSubmit.childNodes).find(node => 
-    node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== ""
+  const btnSubmit = document.getElementById("btn-add-task-submit");
+  const textNode = Array.from(btnSubmit.childNodes).find(
+    (node) => node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== ""
   );
   if (textNode) {
     textNode.textContent = "Ok";
@@ -230,7 +250,7 @@ async function toggleEditTaskDetails(taskId) {
 
 /**
  * Delets a task with the given task id.
- * 
+ *
  * @async
  * @param {string} taskId task id for the tast which should be deleted
  */
@@ -238,51 +258,75 @@ async function doDeleteTask(taskId) {
   await deleteTask(taskId);
   renderTasks();
   toggleTaskDetails();
-  showDeleteMessage()
+  showDeleteMessage();
 }
 
 /**
  * Shows a information message as toast when deleting a task.
  */
 function showDeleteMessage() {
-    document.getElementById("overlay").classList.remove("dNone");
-    overlay.classList.add("animate");
-    setTimeout(function () {
-      overlay.classList.add("dNone");
-    }, 2000);
+  document.getElementById("overlay").classList.remove("dNone");
+  overlay.classList.add("animate");
+  setTimeout(function () {
+    overlay.classList.add("dNone");
+  }, 2000);
 }
 
 function loadTaskDetails(taskId, task, contacts) {
   document.getElementById("title").value = task.title;
   document.getElementById("description").value = task.description;
   document.getElementById("dueDate").value = displayTaskDueDate(task.dueDate);
-  
+
   if (task.prio === "urgent") {
-    document.getElementById("add-task-btn-urgent").classList.add("active", "selected");
+    document
+      .getElementById("add-task-btn-urgent")
+      .classList.add("active", "selected");
   } else {
-    document.getElementById("add-task-btn-urgent").classList.remove("active", "selected");
+    document
+      .getElementById("add-task-btn-urgent")
+      .classList.remove("active", "selected");
   }
   if (task.prio === "medium") {
-    document.getElementById("add-task-btn-medium").classList.add("active", "selected");
+    document
+      .getElementById("add-task-btn-medium")
+      .classList.add("active", "selected");
   } else {
-    document.getElementById("add-task-btn-medium").classList.remove("active", "selected");
+    document
+      .getElementById("add-task-btn-medium")
+      .classList.remove("active", "selected");
   }
   if (task.prio === "low") {
-    document.getElementById("add-task-btn-low").classList.add("active", "selected");
+    document
+      .getElementById("add-task-btn-low")
+      .classList.add("active", "selected");
   } else {
-    document.getElementById("add-task-btn-low").classList.remove("active", "selected");
+    document
+      .getElementById("add-task-btn-low")
+      .classList.remove("active", "selected");
   }
 
-  document.getElementById("categorySelected").setAttribute('data-value', task.category === "userstory" ? "personal" : "work");
-  document.getElementById("category").value = task.category === "userstory" ? "User Story" : "Technical Task";
+  document
+    .getElementById("categorySelected")
+    .setAttribute(
+      "data-value",
+      task.category === "userstory" ? "personal" : "work"
+    );
+  document.getElementById("category").value =
+    task.category === "userstory" ? "User Story" : "Technical Task";
 }
 
 function resetTaskDetails() {
   document.getElementById("title").value = "";
   document.getElementById("description").value = "";
   document.getElementById("dueDate").value = "";
-  
-  document.getElementById("add-task-btn-urgent").classList.remove("active", "selected");
-  document.getElementById("add-task-btn-medium").classList.remove("active", "selected");
-  document.getElementById("add-task-btn-low").classList.remove("active", "selected");
+
+  document
+    .getElementById("add-task-btn-urgent")
+    .classList.remove("active", "selected");
+  document
+    .getElementById("add-task-btn-medium")
+    .classList.remove("active", "selected");
+  document
+    .getElementById("add-task-btn-low")
+    .classList.remove("active", "selected");
 }
--- assets/templates/boardTemplate.js
+++ assets/templates/boardTemplate.js
@@ -1,8 +1,8 @@
 function displayBoardContainer(board) {
-    let addTaskContent = board.addtasks
-        ? `<div onclick="showAddTaskOverlay('${board.id}')" class="board-container-titlebox-addtask d-flex justify-content-center align-items-center">+</div>`
-        : "";
-    return `
+  let addTaskContent = board.addtasks
+    ? `<div onclick="showAddTaskOverlay('${board.id}')" class="board-container-titlebox-addtask d-flex justify-content-center align-items-center">+</div>`
+    : "";
+  return `
         <div class="board-container" ondrop="moveTaskTo('${board.id}')" ondragover="allowDrop(event)">
             <div class="board-container-titlebox d-flex justify-content-between align-items-center">
                 <span class="board-container-titlebox-title f10">${board.name}</span>
@@ -16,16 +16,16 @@ function displayBoardContainer(board) {
 }
 
 function displayBoardTasks(board, tasks, contacts) {
-    const taskCount = board && board.tasks ? Object.keys(board.tasks).length : 0;
-    if (taskCount <= 0) {
-        return displayEmptyBoard(board);
-    } else {
-        return displayTasks(board, tasks, contacts);
-    }
+  const taskCount = board && board.tasks ? Object.keys(board.tasks).length : 0;
+  if (taskCount <= 0) {
+    return displayEmptyBoard(board);
+  } else {
+    return displayTasks(board, tasks, contacts);
+  }
 }
 
 function displayEmptyBoard(board) {
-    return `
+  return `
             <div class="board-tasks-notasks d-flex justify-content-center align-items-center f3">
                 No tasks ${board.name}
             </div>
@@ -33,16 +33,17 @@ function displayEmptyBoard(board) {
 }
 
 function displayTasks(board, tasks, contacts) {
-    let htmlContent = "";
+  let htmlContent = "";
 
-    Object.keys(board.tasks).forEach((taskId) => {
-        const task = tasks[taskId];
-        if (task === undefined || task === null) {
-            return;
-        }
-        htmlContent += `
-            <div onclick="toggleTaskDetails('${taskId}')" class="board-task-container" draggable="true" ondragstart="startTaskDragging('${board.id
-            }', '${taskId}')">
+  Object.keys(board.tasks).forEach((taskId) => {
+    const task = tasks[taskId];
+    if (task === undefined || task === null) {
+      return;
+    }
+    htmlContent += `
+            <div onclick="toggleTaskDetails('${taskId}')" class="board-task-container" draggable="true" ondragstart="startTaskDragging('${
+      board.id
+    }', '${taskId}')">
                 <div class="d-flex mb-24">
                     ${displayTaskType(task.category)}
                 </div>
@@ -59,32 +60,33 @@ function displayTasks(board, tasks, contacts) {
                     <div class="board-task-contacts">
                         ${displayAssignedTo(task, contacts)}
                     </div>
-                    <img class="board-task-category" src="./assets/icons/prio-${task.prio
-            }.svg" alt="">
+                    <img class="board-task-category" src="./assets/icons/prio-${
+                      task.prio
+                    }.svg" alt="">
                 </div>
             </div>
         `;
-    });
-    return htmlContent;
+  });
+  return htmlContent;
 }
 
 function displayTaskType(taskCategory) {
-    if ("userstory" === taskCategory) {
-        return `<div class="board-task-type task-type-userstory f3">User Story</div>`;
-    } else {
-        return `<div class="board-task-type task-type-technicaltask f3">Technical Task</div>`;
-    }
+  if ("userstory" === taskCategory) {
+    return `<div class="board-task-type task-type-userstory f3">User Story</div>`;
+  } else {
+    return `<div class="board-task-type task-type-technicaltask f3">Technical Task</div>`;
+  }
 }
 
 function displaySubTasks(task) {
-    const subTasksCount =
-        task && task.subtasks ? Object.keys(task.subtasks).length : 0;
-    if (subTasksCount <= 0) {
-        return "";
-    } else {
-        const subTasksDone = getSubTasksDoneCount(task, subTasksCount);
-        const subTasksDonePercent = (subTasksDone / subTasksCount) * 100;
-        return `
+  const subTasksCount =
+    task && task.subtasks ? Object.keys(task.subtasks).length : 0;
+  if (subTasksCount <= 0) {
+    return "";
+  } else {
+    const subTasksDone = getSubTasksDoneCount(task, subTasksCount);
+    const subTasksDonePercent = (subTasksDone / subTasksCount) * 100;
+    return `
             <div class="board-task-subtasks mb-24 d-flex align-items-center">
                 <div class="board-subtasks-bar">
                     <div style="width: ${subTasksDonePercent}%" class="board-subtasks-bar-value"></div>
@@ -92,42 +94,54 @@ function displaySubTasks(task) {
                 <span id="class="f11">${subTasksDone}/${subTasksCount} Subtasks</span>
             </div>
         `;
-    }
+  }
 }
 
 function getSubTasksDoneCount(task, subTasksCount) {
-    let subTasksDoneCount = 0;
-    for (let subTaskIdx = 0; subTaskIdx < subTasksCount; subTaskIdx++) {
-        if (task.subtasks["subtask" + (subTaskIdx + 1)].done) {
-            subTasksDoneCount++;
-        }
+  let subTasksDoneCount = 0;
+  for (let subTaskIdx = 0; subTaskIdx < subTasksCount; subTaskIdx++) {
+    if (task.subtasks["subtask" + (subTaskIdx + 1)].done) {
+      subTasksDoneCount++;
     }
-    return subTasksDoneCount;
+  }
+  return subTasksDoneCount;
 }
 
 function displayAssignedTo(task, contacts) {
-    const contactsCount =
-        task && task.assignedTo ? Object.keys(task.assignedTo).length : 0;
-    if (contactsCount <= 0) {
-        return "";
-    } else {
-        let contactsContent = "";
-        Object.keys(task.assignedTo).forEach((contactId) => {
-            const contact = contacts[contactId];
-            if (contact === undefined || contact === null) {
-                return;
-            } else {
-                contactsContent += displayContact(contact);
-            }
-        });
+  const contactsCount =
+    task && task.assignedTo ? Object.keys(task.assignedTo).length : 0;
+  if (contactsCount <= 0) {
+    return "";
+  } else {
+    let contactsContent = "";
+    Object.keys(task.assignedTo).forEach((contactId) => {
+      const contact = contacts[contactId];
+      if (contact === undefined || contact === null) {
+        return;
+      } else {
+        contactsContent += displayContact(contact);
+      }
+    });
 
-        return contactsContent;
-    }
+    return contactsContent;
+  }
 }
 
 function displayContact(contact) {
-    return `
-        <div class="task-contact f11" style="background: ${contact.avatarColor
+  return `
+        <div class="task-contact f11" style="background: ${
+          contact.avatarColor
         }">${getShortcutName(contact)}</div >
         `;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  function checkScreenSize() {
+    if (window.innerWidth >= 1081) {
+      document.querySelector(".respX").style.display = "none";
+    }
+  }
+
+  checkScreenSize();
+  window.addEventListener("resize", checkScreenSize);
+});
